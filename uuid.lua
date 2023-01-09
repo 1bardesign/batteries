f5be54d50fe9d8404e8d510c466fa3a14c95a0b8
rhy <81539300+rhynomatt@users.noreply.github.com>
2023-01-09 12:41:02 -0300

    uuid: Slight style fix
    Don't need to capture the 2nd value oops, also separated the format
    call into multiple lines

--- uuid.lua
+++ uuid.lua
@@ -19,8 +19,11 @@ local uuid4_template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
 function uuid.uuid4(rng)
 	-- x should be 0x0-0xF, the single y should be 0x8-0xB
 	-- 4 should always just be 4 (denoting uuid version)
-    local out, _ = uuid4_template:gsub("[xy]", function (c)
-        return string.format("%x", c == "x" and _random(rng, 0x0, 0xF) or _random(rng, 0x8, 0xB))
+    local out = uuid4_template:gsub("[xy]", function (c)
+        return string.format(
+			"%x",
+			c == "x" and _random(rng, 0x0, 0xF) or _random(rng, 0x8, 0xB)
+		)
     end)
 
 	return out

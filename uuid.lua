b8d1badf83d845281c0ac6275bfd5503ac17d4cd
rhy <81539300+rhynomatt@users.noreply.github.com>
2023-01-09 12:23:11 -0300

    uuid: Discard gsub's 2nd return value

--- uuid.lua
+++ uuid.lua
@@ -17,11 +17,13 @@ local uuid4_template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
 
 --generate a UUID version 4 (random)
 function uuid.uuid4(rng)
-    return uuid4_template:gsub("[xy]", function (c)
-        -- x should be 0x0-0xF, the single y should be 0x8-0xB
-        -- 4 should always just be 4 (denoting uuid version)
+	-- x should be 0x0-0xF, the single y should be 0x8-0xB
+	-- 4 should always just be 4 (denoting uuid version)
+    local out, _ = uuid4_template:gsub("[xy]", function (c)
         return string.format("%x", c == "x" and _random(rng, 0x0, 0xF) or _random(rng, 0x8, 0xB))
     end)
+
+	return out
 end
 
 return uuid
